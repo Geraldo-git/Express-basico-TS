a9de3ea76e86a66e4cbde3f8aa46ae427b1c6409
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-08 06:51:44 -0300

    Estrutura e imports iniciais

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
+import express from "express";
+const app = express();
 
-console.log("Express + TS");
+app.get('/', (req,res)=>{
+res.send('<h1> Hello express<h1>');
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log(" Aplicacao escutando porta 3000");
+});
